cee42abdf0ed8c4255dd9b86d7f057cf9e7e9544
Peter Chen <29876364+cwz0522@users.noreply.github.com>
2019-08-22 19:32:54 +0800

    Minor fix for previous commit

--- Wii/IEGS2013/md2html.sh
+++ Wii/IEGS2013/md2html.sh
@@ -1,2 +1,2 @@
 pandoc -f gfm -t html5 -s -c ../../github-pandoc.css -o inazuma-eleven-go-strikers-2013-guide.html inazuma-eleven-go-strikers-2013-guide.md 
-pandoc -f gfm -t html5 -s -c ../../github-pandoc.css -o inazuma-eleven-go-strikers-2013-all-special-moves.html inazuma-eleven-go-strikers-2013-guide.md 
+pandoc -f gfm -t html5 -s -c ../../github-pandoc.css -o inazuma-eleven-go-strikers-2013-all-special-moves.html inazuma-eleven-go-strikers-2013-all-special-moves.md
